73dcd1b4a9150589c8c62afb8c678bce3ec6c13b
Michael Pautov <123prove123@gmail.com>
2017-02-01 14:48:58 +0600

    update cloudtrail

--- services/config.rb
+++ services/config.rb
@@ -343,7 +343,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-aws" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.7.2"
+                   :version => "1.7.3"
                }       ])
   json_input '{ "composite name":"PLAN::stack_name",
       "plan name":"PLAN::name",
