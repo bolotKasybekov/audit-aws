41a8803b41cbc0c77463b1c7ecf8d0ecb69df78b
Paul Allen <pauldallen@gmail.com>
2017-03-16 16:14:56 -0700

    updating jsrunner version to use the variable

--- services/config.rb
+++ services/config.rb
@@ -202,7 +202,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-aws" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.9.2"
+                   :version => "${JSRUNNER_COMMONS_VERSION}"
                },
                {
                    :name => "js-yaml",
