d28a1cc88bb9da2078bc09dd612e4e744defbdfd
Paul Allen <pauldallen@gmail.com>
2017-03-17 13:10:56 -0700

    upping jsrunner-commons to 1.10.7-9

--- services/config.rb
+++ services/config.rb
@@ -202,7 +202,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-aws" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.9.2"
+                   :version => "1.10.7-9"
                },
                {
                    :name => "js-yaml",
