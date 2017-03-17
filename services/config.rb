73bd61a96df3ede1ec0f4d7d57b92e12d9b04c13
Michael Pautov <michael.pautov@gmail.com>
2017-03-17 10:37:08 +0600

    Changed JSRunner to 1.9.6-beta1

--- services/config.rb
+++ services/config.rb
@@ -223,7 +223,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-aws" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.9.6"
+                   :version => "1.9.6-beta1"
                },
                {
                    :name => "js-yaml",
