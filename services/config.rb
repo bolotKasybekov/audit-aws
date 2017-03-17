eed4bbf03da0ded6db981d66fd04dc1d71379b61
Paul Allen <pauldallen@gmail.com>
2017-03-16 17:12:00 -0700

    un-variablizing jsrunner-commons

--- services/config.rb
+++ services/config.rb
@@ -202,7 +202,7 @@ coreo_uni_util_jsrunner "tags-to-notifiers-array-aws" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "${JSRUNNER_COMMONS_VERSION}"
+                   :version => "1.9.2"
                },
                {
                    :name => "js-yaml",
